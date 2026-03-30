f6b502ffbe5857117f5549569685c44aabf2a684
Vedran Kopanja <vedran@kopanja.rs>
2026-03-30 21:25:47 +0200

    Add decoupling, lesson CH3L7

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -26,24 +27,48 @@ func main() {
 
 	fmt.Println("RabbitMQ connection successful!")
 
+	gamelogic.PrintClientHelp()
+	for {
+		input := gamelogic.GetInput()
+		breakFromRepl := false
+
+		switch input[0] {
+		case "pause":
+			fmt.Println("sending pause message")
+			sendMessage(conn, true)
+		case "resume":
+			fmt.Println("sending resume message")
+			sendMessage(conn, false)
+		case "quit":
+			fmt.Println("exiting...")
+			ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+			defer cancel()
+			ctx.Done()
+
+			fmt.Println("Shutting down gracefully...")
+
+			_, shutdownCancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer shutdownCancel()
+			breakFromRepl = true
+		default:
+			fmt.Println("unknown command")
+		}
+
+		if breakFromRepl {
+			break
+		}
+	}
+}
+
+func sendMessage(conn *amqp.Connection, isPaused bool) {
 	chn, err := conn.Channel()
 	if err != nil {
 		panic(fmt.Sprintf("There was an error opening the channel: %v", err))
 	}
 
 	playState := routing.PlayingState{
-		IsPaused: true,
+		IsPaused: isPaused,
 	}
 
 	pubsub.PublishJSON(chn, routing.ExchangePerilDirect, routing.PauseKey, playState)
-
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer cancel()
-
-	<-ctx.Done()
-
-	fmt.Println("Shutting down gracefully...")
-
-	_, shutdownCancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer shutdownCancel()
 }
